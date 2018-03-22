29af0d388255ab488b62c8966e6f1d9f03532e13
razdrian <raz.adrian95@gmail.com>
2018-03-22 22:22:12 +0200

    The one from linux before reorganisation

--- sources/py/tmp/httpTes.py
+++ sources/py/tmp/httpTes.py
@@ -1,19 +1,7 @@
 import subprocess
+# result = subprocess.run(['echo', 'da'], stdout=subprocess.PIPE)
+# print (result.stdout)
 
+process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+result, error = process.communicate('ping ' + '-w ' + '1 ' + '-W ' + '3 ' + 'smtp.gmail.com')
 
-# process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# result, error = process.communicate('ping ' + '-w ' + '1 ' + '-W ' + '3 ' + '8.8.8.8')
-
-try:
-    process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    result,error=process.communicate(' find /var/smsgate/input/ -name \'*\'')
-    result = result.split('\n')
-
-except:
-    print ('error now')
-else:
-    print len(result)
-    if(len(result)>2):
-        print ('there is someone there')
-    else:
-        print ('all is void there man')
